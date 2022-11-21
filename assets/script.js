d44f6c765c445d4045d686709f0a072c73099f14
Laura Trapaga <trapaga9@gmail.com>
2022-11-21 00:24:53 -0500

    edited javascript code

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,41 @@
+
 $(document).ready(function() {
 
-  $("#currentDay").text(dayjs().format("MMM "+ "DD, " + "YYYY"));
+$("#currentDay").text(dayjs().format("MMM "+ "DD, " + "YYYY"));
 
-  function currentTime() {
-    var current = dayjs().hour();
-  
-    $(".time-block").each(function() {
+function currentTime() {
+  var current = dayjs().hour();
+      $(".time-block").each(function() {
       var hour = parseInt($(this).attr("id"));
-
-      if (hour > current) {
-          $(this).addClass("future");
-      } else if (current === hour) {
+        if (hour > current) {
+        $(this).addClass("future");
+      }   else if (current === hour) {
           $(this).addClass("present");
-      } else {
-          $(this).addClass("past");
+      }     else {
+            $(this).addClass("past");
       }
   })
 };
 
-currentTime();
+var saveBtn = $(".saveBtn")
+
+saveBtn.on("click", function() {
+  var time = $(this).siblings(".hour").text();
+  var input = $(this).siblings(".description").val()
+  localStorage.setItem(time, input);
 });
 
+function saveData() {
+  $(".hour").each(function() {
+    var currTime = $(this).text();
+    var currInput = localStorage.getItem(currTime);
+      if(currInput !== null) {
+      $(this).siblings(".description").val(currInput);
+    }
+  });
+}
 
+currentTime();
+saveData();
+
+});
