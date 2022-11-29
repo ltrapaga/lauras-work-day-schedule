85fd5d4e9fba36e650499ca2bfceaef3ff0b64cd
Laura Trapaga <trapaga9@gmail.com>
2022-11-28 21:25:32 -0500

    add comment to JS code

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,11 @@
-
+// Code included inside $( document ).ready() will only run once the page DOM is ready for JavaScript code to execute.
 $(document).ready(function() {
 
+// Retrieves current date from Day.js. Diplays the date in the the html <p> with the id "currentDay".
 $("#currentDay").text(dayjs().format("MMM "+ "DD, " + "YYYY"));
 
+/* Retrieves current hour from Day.js (0-23). Compares the hour of each time block with the current hour 
+to determine which class (past, present, or future) will be added to which time block. */
 function currentTime() {
   var current = dayjs().hour();
       $(".time-block").each(function() {
@@ -17,8 +20,11 @@ function currentTime() {
   })
 };
 
+// Defines variable for saveBtn.
 var saveBtn = $(".saveBtn")
 
+/* Creates data items inside local storage when the saveBtn is clicked. Displays the text that was saved 
+to local storage in the textarea.description when the page is refreshed. */
 saveBtn.on("click", function() {
   var time = $(this).siblings(".hour").text();
   var input = $(this).siblings(".description").val()
@@ -35,6 +41,7 @@ function saveData() {
   });
 }
 
+// Calls currentTime and saveData functions.
 currentTime();
 saveData();
 
